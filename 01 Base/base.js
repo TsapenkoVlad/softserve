bb6c8844639a44f75c15d2948f49bb1f0fe2032c
TsapenkoVlad <vlad.czapenko5@gmail.com>
2023-06-13 20:32:44 +0300

    first commit

---
+++ 01 Base/base.js
@@ -0,0 +1,62 @@
+let surname = "Tsapenko";
+
+console.log(surname);
+
+
+let number = 2;
+let number2 = 3;
+
+console.log(number);
+console.log(number2);
+number = number2;
+console.log(number);
+
+let primitiveTypes = {
+    string: "Hello, world!",
+    number: 42,
+    boolean: true,
+    undefined: undefined,
+    null: null
+  };
+  
+
+let isAdult = confirm("Вы достигли совершеннолетия?");
+console.log(isAdult);
+
+
+let firstName = 'Vlad';
+let LastName = 'Tsapenko';
+let group = 'JavascriptFundametals';
+let birthYear = 1999;
+let isMarried = false;
+
+console.log(typeof birthYear, birthYear);
+console.log(typeof isMarried, isMarried);
+console.log(typeof firstName, firstName);
+
+let nullVariable = null;
+let undefinedVariable;
+
+console.log(typeof nullVariable, nullVariable);
+console.log(typeof undefinedVariable, undefinedVariable);
+
+
+
+let login = prompt('your login?')
+let email = prompt('your email?')
+let password = prompt('your password?')
+
+alert('Dear User, your email is ' + email + ', your password is ' + password);
+
+
+let  SecondInHour = 60 * 60;
+let  SecondInDay = SecondInHour * 24 ;
+let  SecondInMonth = SecondInDay * 30;
+
+console.log(SecondInHour);
+console.log(SecondInDay);
+console.log(SecondInMonth);
+
+
+
+
