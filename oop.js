0a7e3b0bf2bba10c13db65ff0e875e6fa46c5f94
TsapenkoVlad <vlad.czapenko5@gmail.com>
2023-07-02 22:32:56 +0300

    oop homework

--- oop.js
+++ oop.js
@@ -27,3 +27,137 @@ class Circle {
     }
 }
 
+let mentor = { 
+    course: "JS fundamental", 
+    duration: 3,
+    direction: "web-development" 
+};
+
+function propsCount(currentObject) {
+    return Object.keys(currentObject).length;
+}
+
+
+class Person {
+    constructor(name, surname) {
+      this.name = name;
+      this.surname = surname;
+    }
+  
+    showFullName() {
+      console.log(`${this.surname} ${this.name} `);
+    }
+  }
+  
+  class Student extends Person {
+    constructor(name, surname, year) {
+      super(name, surname);
+      this.year = year;
+    }
+  
+    showFullName(middleName) {
+      console.log(`${this.surname} ${this.name} ${middleName}`);
+    }
+  
+    showCourse() {
+      console.log(new Date().getFullYear() - this.year);
+    }
+  }
+  
+  const student = new Student("Ivan", "Ivanov", 2019);
+   student.showFullName("Ivanovich");
+   student.showCourse();
+
+
+
+   class Marker {
+    constructor(color, qtyOfPaint) {
+      this.color = color;
+      this.qtyOfPaint = qtyOfPaint;
+    }
+  
+    print(text) {
+      while (this.qtyOfPaint > 0) {
+        console.log(`%c${text}`, `color: ${this.color};`);
+        const lengthWithoutWhitespaces = text.replace(" ", "").length;
+        this.qtyOfPaint = this.qtyOfPaint - 0.5 * lengthWithoutWhitespaces;
+      }
+    }
+  }
+  
+   const marker = new Marker("red", 10);
+   marker.print("Hello");
+  
+  class RefillingMarker extends Marker {
+    constructor(color, qtyOfPaint) {
+      super(color, qtyOfPaint);
+    }
+  
+    refill(newPaint) {
+      this.qtyOfPaint = this.qtyOfPaint + newPaint;
+    }
+  }
+  
+  // const refillMarker = new RefillingMarker("red", 10);
+  // refillMarker.print("Hello");
+  // console.log("refillMarker.qtyOfPaint", refillMarker.qtyOfPaint);
+  // refillMarker.refill(5);
+  // console.log("refillMarker.qtyOfPaint", refillMarker.qtyOfPaint);
+
+  
+
+  class Worker {
+    #experience = 1.2;
+    constructor(fullName, dayRate, workingDays) {
+      this.fullName = fullName;
+      this.dayRate = dayRate;
+      this.workingDays = workingDays;
+    }
+  
+    showSalary() {
+      console.log("Salary ", this.dayRate * this.workingDays);
+    }
+  
+    showSalaryWithExperience() {
+     return  this.dayRate * this.workingDays * this.#experience;
+    }
+  
+    getExperience() {
+      return this.#experience;
+    }
+  
+    setExperience(newExperience) {
+      this.#experience = newExperience;
+    }
+  
+    static sortWorkers(arrayExperience) {
+      return arrayExperience.sort((a, b) => a.getExperience() - b.getExperience());
+    }
+    
+  }
+  
+  const worker = new Worker("Ivan Smith"  , 20, 20);
+  worker.showSalary();
+  worker.showSalaryWithExperience();
+  
+  console.log("experience", worker.getExperience());
+  worker.setExperience(1.5);
+  console.log("experience", worker.getExperience());
+  
+  worker.showSalary();
+  worker.showSalaryWithExperience();
+  
+  let worker2 = new Worker("Tom Tomson", 48, 22);
+  
+  let worker3 = new Worker("Andrey Simpson", 12, 23);
+  
+  const arrayExperience = [worker, worker2, worker3];
+  
+  const sortedArray = Worker.sortWorkers(arrayExperience);
+  
+console.log('Sorted salary:');
+
+  sortedArray.forEach((worker) => {
+    console.log(worker.fullName + ': ' + worker.showSalaryWithExperience());
+})
+ 
